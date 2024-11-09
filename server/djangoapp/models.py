21a792757040848b4f0ba58a5d2d5e37c3ee9b61
danica-zhu <145221361+danica-zhu@users.noreply.github.com>
2024-11-09 16:16:32 -0500

    add carmodel and car make

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,20 @@
+# pylint: disable=missing-class-docstring
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)  # 汽车品牌名称
+    description = models.TextField()  # 描述信息
+    country = models.CharField(max_length=50, blank=True, null=True)  # 制造国家
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"  # 返回名称和国家作为字符串表示
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
@@ -15,6 +22,29 @@
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    # dealer_id = models.IntegerField()  # 经销商ID，参考Cloudant数据库中的经销商
+    dealer_id = models.IntegerField(null=True, blank=True) 
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    # Other fields as needed
+
+    def __str__(self):
+        # return self.name  # Return the name as the string representation
+        return f"{self.car_make.name} {self.name} ({self.year})" 
+
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
 # - Name
