b6d9e202873eec6f6617107cb0616897af6537eb
danica-zhu <145221361+danica-zhu@users.noreply.github.com>
2024-11-10 16:10:23 -0500

    new2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,7 +3,7 @@
 # dsadsadsadasdsadasda
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+# change
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
